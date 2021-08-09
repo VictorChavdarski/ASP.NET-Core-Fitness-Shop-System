bb4bcdceeafc4107cbbedd74b691e04798ac05ef
Victor Chavdarski <kingvicho99@gmail.com>
2021-08-09 19:34:40 +0300

    Implemented admin area

---
+++ FitnessShopSystem/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿namespace FitnessShopSystem.Areas.Admin.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Authorization;
+
+    using static AdminConstants;
+
+    [Area(AreaName)]
+    [Authorize(Roles = AdministratorRoleName)]
+    public abstract class AdminController : Controller
+    {
+    }
+}
