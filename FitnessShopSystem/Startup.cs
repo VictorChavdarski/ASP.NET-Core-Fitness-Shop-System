b98b5ce882854dc6d2583cad9c1356b3a92275f7
Victor Chavdarski <kingvicho99@gmail.com>
2021-08-10 13:44:36 +0300

    Edited index view

--- FitnessShopSystem/Startup.cs
+++ FitnessShopSystem/Startup.cs
@@ -74,10 +74,7 @@ namespace FitnessShopSystem
                 .UseAuthorization()
                 .UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "Areas",
-                    pattern: "/{area:exists}/{controller=Home}/{action=Index}/{id?}");
-
+                endpoints.MapDefaultAreaRoute();
                 endpoints.MapDefaultControllerRoute();
                 endpoints.MapRazorPages();
             });
